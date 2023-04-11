cfaf4f2a659f82f575641b05b6cb914b030f96cd
alicia-knauer <alicia.knauer@gmx.de>
2023-04-12 01:16:54 +0200

    Update steelrack.py

--- Examples/steelrack.py
+++ Examples/steelrack.py
@@ -61,73 +61,74 @@ if __name__ == '__main__':
         Point(i+56, m[i]*0.064, 0.062+t*n[i])
         Point(i+60, m[i]*0.0685+t*n[i],0.0575)
         Point(i+64, m[i]*0.0685+t*n[i], 0.0295)
-        Point(i+52, m[i]*0.0659, 0.0277)
-        Point(i+54, m[i]*0.0671, 0.0249)
-        Point(i+56, m[i]*0.0645+t*n[i], 0.0232)
-        Point(i+58, m[i]*0.0645+t*n[i], 0.0138)
-        Point(i+60, m[i]*0.0671, 0.0121)
-        Point(i+62, m[i]*0.0659, 0.0093)
-        Point(i+64, m[i]*0.0685+t*n[i], 0.0075)
-        Point(i+66, m[i]*0.0685+t*n[i], 0.0025)
-        Point(i+68, m[i]*0.063, -0.003+t*n[i])
-        Point(i+70, m[i]*0.0061, -0.003+t*n[i])
-        Point(i+72, m[i]*0.0024, -0.0027)
-        Point(i+74, m[i]*0.0048, -0.0007)
+        Point(i+66, m[i]*0.0659, 0.0277)
+        Point(i+70, m[i]*0.0671, 0.0249)
+        Point(i+74, m[i]*0.0645+t*n[i], 0.0232)
+        Point(i+78, m[i]*0.0645+t*n[i], 0.0138)
+        Point(i+82, m[i]*0.0671, 0.0121)
+        Point(i+86, m[i]*0.0659, 0.0093)
+        Point(i+90, m[i]*0.0685+t*n[i], 0.0075)
+        Point(i+94, m[i]*0.0685+t*n[i], 0.0025)
+        Point(i+98, m[i]*0.063, -0.003+t*n[i])
+        Point(i+102, m[i]*0.0061, -0.003+t*n[i])
+        Point(i+106, m[i]*0.0024, -0.0027)
+        Point(i+110, m[i]*0.0048, -0.0007)
         
-    Point(100, 0+t, 0)
-    Point(101, 0-t, 0)
+    Point(112, 0, 0+t)
+    Point(113, 0, 0-t)
 
-
+    Line(29, '36 38')
+    Line(30, '37 39')
     # Creating Lines     
-    # for i in range(2):
-    #     Line(i+1, str(i+1)+' '+ str(i+3))
-    #     Line(i+3, str(i+5)+' '+ str(i+7))
-    #     Line(i+5, str(i+9)+' '+ str(i+11))
-    #     Line(i+7, str(i+13)+' '+ str(i+15))
-    #     Line(i+9, str(i+19)+' '+ str(i+21))
-    #     Line(i+11, str(i+25)+' '+ str(i+27))
-    #     Line(i+13, str(i+29)+' '+ str(i+31))
+    for i in range(4):
+        Line(i+1, str(i+36)+' '+ str(i+40))
+        Line(i+5, str(i+44)+' '+ str(i+48))
+        Line(i+9, str(i+52)+' '+ str(i+56))
+        Line(i+13, str(i+60)+' '+ str(i+64))
+        Line(i+17, str(i+74)+' '+ str(i+78))
+        Line(i+21, str(i+90)+' '+ str(i+94))
+        Line(i+25, str(i+98)+' '+ str(i+102))
 
-    # for i in range(2):
-    #     Line.Arc(15+i, [3+i, 5+i], [m[i]*0.0336, 0.0924])
-    #     Line.Arc(17+i, [7+i, 9+i], [m[i]*0.0333, 0.0633])
-    #     Line.Arc(19+i, [11+i, 13+i], [m[i]*0.0672, 0.0607])
-    #     Line.Arc(21+i, [15+i, 17+i], [m[i]*0.068, 0.0276])
-    #     Line.Arc(23+i, [17+i, 19+i], [m[i]*0.065, 0.025])
-    #     Line.Arc(25+i, [21+i, 23+i], [m[i]*0.065, 0.012])
-    #     Line.Arc(27+i, [23+i, 25+i], [m[i]*0.068, 0.0094])
-    #     Line.Arc(29+i, [27+i, 29+i], [m[i]*0.0669, -0.0014])
-    #     Line.Arc(31+i, [31+i, 33+i], [m[i]*0.0047, -0.0027])
+    for i in range(2):
+        Line.Arc(29+i, [40+i, 44+i], [m[i]*0.0336, 0.0924])
+        Line.Arc(33+i, [48+i, 52+i], [m[i]*0.0333, 0.0633])
+        Line.Arc(37+i, [56+i, 60+i], [m[i]*0.0672, 0.0607])
+        Line.Arc(41+i, [64+i, 68+i], [m[i]*0.068, 0.0276])
+        Line.Arc(45+i, [68+i, 74+i], [m[i]*0.065, 0.025])
+        Line.Arc(49+i, [78+i, 88+i], [m[i]*0.065, 0.012])
+        Line.Arc(53+i, [88+i, 90+i], [m[i]*0.068, 0.0094])
+        Line.Arc(57+i, [94+i, 98+i], [m[i]*0.0669, -0.0014])
+        Line.Arc(61+i, [102+i, 110+i], [m[i]*0.0047, -0.0027])
     
-    # Line.Arc(33, [33, 34], [0,0])
+    Line.Arc(33, [33, 34], [0,0])
 
 
     thickness1 = 0.003
     # Creating Elements
-    for i in range(2):
-        Element(1 +i, [1+i, 3+i], thickness=thickness1)
-        Element(3+i, [5+i, 7+i], thickness=thickness1)
-        Element(5+i, [9+i, 11+i], thickness=thickness1)
-        Element(7+i, [13+i, 15+i], thickness=thickness1)
-        Element(9+i, [19+i, 21+i], thickness=thickness1)
-        Element(11+i, [25+i, 27+i], thickness=thickness1)
-        Element(13+i, [29+i, 31+i], thickness=thickness1)
+    # for i in range(2):
+    #     Element(1 +i, [1+i, 3+i], thickness=thickness1)
+    #     Element(3+i, [5+i, 7+i], thickness=thickness1)
+    #     Element(5+i, [9+i, 11+i], thickness=thickness1)
+    #     Element(7+i, [13+i, 15+i], thickness=thickness1)
+    #     Element(9+i, [19+i, 21+i], thickness=thickness1)
+    #     Element(11+i, [25+i, 27+i], thickness=thickness1)
+    #     Element(13+i, [29+i, 31+i], thickness=thickness1)
 
-    for i in range(2):
-        Element.Arc(15+i, [3+i, 5+i], [m[i]*0.0336, 0.0924], thickness=thickness1)
-        Element.Arc(17+i, [7+i, 9+i], [m[i]*0.0333, 0.0633], thickness=thickness1)
-        Element.Arc(19+i, [11+i, 13+i], [m[i]*0.0672, 0.0607], thickness=thickness1)
-        Element.Arc(21+i, [15+i, 17+i], [m[i]*0.068, 0.0276], thickness=thickness1)
-        Element.Arc(23+i, [17+i, 19+i], [m[i]*0.065, 0.025], thickness=thickness1)
-        Element.Arc(25+i, [21+i, 23+i], [m[i]*0.065, 0.012], thickness=thickness1)
-        Element.Arc(27+i, [23+i, 25+i], [m[i]*0.068, 0.0094], thickness=thickness1)
-        Element.Arc(29+i, [27+i, 29+i], [m[i]*0.0669, -0.0014], thickness=thickness1)
-        Element.Arc(31+i, [31+i, 33+i], [m[i]*0.0047, -0.0027], thickness=thickness1)
+    # for i in range(2):
+    #     Element.Arc(15+i, [3+i, 5+i], [m[i]*0.0336, 0.0924], thickness=thickness1)
+    #     Element.Arc(17+i, [7+i, 9+i], [m[i]*0.0333, 0.0633], thickness=thickness1)
+    #     Element.Arc(19+i, [11+i, 13+i], [m[i]*0.0672, 0.0607], thickness=thickness1)
+    #     Element.Arc(21+i, [15+i, 17+i], [m[i]*0.068, 0.0276], thickness=thickness1)
+    #     Element.Arc(23+i, [17+i, 19+i], [m[i]*0.065, 0.025], thickness=thickness1)
+    #     Element.Arc(25+i, [21+i, 23+i], [m[i]*0.065, 0.012], thickness=thickness1)
+    #     Element.Arc(27+i, [23+i, 25+i], [m[i]*0.068, 0.0094], thickness=thickness1)
+    #     Element.Arc(29+i, [27+i, 29+i], [m[i]*0.0669, -0.0014], thickness=thickness1)
+    #     Element.Arc(31+i, [31+i, 33+i], [m[i]*0.0047, -0.0027], thickness=thickness1)
     
-    Element.Arc(33, [33, 34], [0, 0], thickness=thickness1)
+    # Element.Arc(33, [33, 34], [0, 0], thickness=thickness1)
 
-    # Creating Part
-    Part(1, '1-33')    
+    # # Creating Part
+    # Part(1, '1-33')    
 
     Model.clientModel.service.finish_modification()
 
